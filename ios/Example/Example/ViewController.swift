2d6128ef90fb5c93acb8875254fcc96579d1f5f1
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-11-04 21:59:44 +0100

    feat(ios): added clean ui kit example app

---
+++ ios/Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by tsinis on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
